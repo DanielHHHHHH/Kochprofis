04777dfb84d6b8ff606b3b4aaa09fb3d8d7e9b11
julianhermle <60342199+julianhermle@users.noreply.github.com>
2020-10-03 19:55:06 +0200

    Schönheitskorrekturen

--- registerScript.js
+++ registerScript.js
@@ -1,30 +1,44 @@
+// JavaScript für register.html
 
 const button = document.getElementById("register");
-
 const txtUsername = document.getElementById("benutzername");
-
 const txtPassword = document.getElementById("kennwort");
 const txtPasswordConfirm = document.getElementById("kennwortConfirm");
 
-
-var txtValue = txtUsername.value;
-
-console.log("Hallo");
-console.log(txtPassword.value);
-
-
-
 button.addEventListener("click", (evt) => {
 
+    //Abfrage ob Textfeld "Username" leer ist
     if (txtUsername.value == "") {
         alert("Bitte erst Benutzername eingeben");
     } else {
         alert("Sie haben einen Benutzername eingegeben");
     }
 
+    //Abfrage ob Textfeld "Password" leer ist
+    if (txtPassword.value == "") {
+        alert("Bitte erst Passwort eingeben");
+    } else {
+        alert("Sie haben ein Passwort eingegeben");
+    }
+
+    //Abfrage ob Textfeld "PasswordConfirm" leer ist
+    if (txtPasswordConfirm.value == "") {
+        alert("Bitte erst Passwort bestätigen");
+    } else {
+        alert("Sie haben ein Passwort bestätigt");
+    }
+
+    //Abfrage ob die eingegebenen Passwörter übereinstimmen
+    if (txtPassword.value == txtPasswordConfirm.value) {
+        alert("alles richtig gemacht");
+    } else {
+        alert("nicht übereinstimmend");
+    }
+
     evt.preventDefault();
 });
 
+/*
 button.addEventListener("click", (evt) => {
 
     if (txtPassword.value == "") {
@@ -55,4 +69,4 @@ button.addEventListener("click", (evt) => {
     }
     evt.preventDefault();
 });
-
+*/
--- startScript.js
+++ startScript.js
@@ -1,21 +1,31 @@
+// JavaScript für start.html
 
 const button = document.getElementById("anmelden");
-
 const txtUsername = document.getElementById("benutzername");
 const txtPassword = document.getElementById("kennwort");
 
 
 button.addEventListener("click", (evt) => {
 
+    // Abfrage ob Textfeld "Username" leer ist
     if (txtUsername.value == "") {
         alert("Bitte erst Benutzername eingeben");
     } else {
         alert("Sie haben einen Benutzername eingegeben");
     }
 
+    // Abfrage ob Textfeld "Password" leer ist
+    if (txtPassword.value == "") {
+        alert("Bitte erst Passwort eingeben");
+    } else {
+        alert("Sie haben ein Passwort eingegeben");
+    }
+
+
     evt.preventDefault();
 });
 
+/*
 button.addEventListener("click", (evt) => {
 
     if (txtPassword.value == "") {
@@ -26,4 +36,4 @@ button.addEventListener("click", (evt) => {
 
     evt.preventDefault();
 });
-
+*/
