8bf1f57994197c060c1d58569df07da5a81f4d28
MarcelMahlfeld <marcel.mahlfeld@gmx.net>
2020-10-06 16:29:05 +0200

    buttons bearbeiten/löschen struktur

--- script.js
+++ script.js
@@ -25,18 +25,23 @@ button.addEventListener("click", (evt) => {
         alert("Ihr Rezept wurde übernommen");
     }
 
-});
 
 //Button bearbeiten
 const button = document.querySelector('#bearbeiten');
 
 button.addEventListener("click", (evt) => {
+    if (txtrezeptText.value == "" or txtrezeptText) /*alter Text gleich neuer Text noch einfügen*/ 
+    {
+        alert("Bitte erst den Text ändern");
+    } else {
+        alert("Die Änderung wurde übernommen");
+    }
 
-});
 
 //Button löschen
 const button = document.querySelector('#löschen');
 
 button.addEventListener("click", (evt) => {
-
+    
+    alert("Das Rezept wurde gelöscht");
 });
