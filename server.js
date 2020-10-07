92dfad03d4b67bbdc22cc58d560528335749f6d1
julianhermle <60342199+julianhermle@users.noreply.github.com>
2020-10-07 16:50:38 +0200

    express-session and dotenv added

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const mysql = require("mysql");
+const session = require("express-session");
 const app = express();
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -15,5 +19,5 @@ app.use(express.static("public"));
 app.listen(2222);
 
 app.get("/", (req, res)=>{
-   res.send("Hello world");
+   //res.send("Hello world");
 });
