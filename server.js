516ebde998a6c2b439af396b08d30871e7634995
Gianna <gdihpol@gmail.com>
2020-10-07 15:26:20 +0200

    server.js datenbank

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'kochprofis'
+  });
+  
 app.use(express.static("public"));
+
+
+
 app.listen(2222);
 
 app.get("/", (req, res)=>{
