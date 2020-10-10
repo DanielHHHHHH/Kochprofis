dca9f3f2775cfde8d21170d454adfb6f4058f6da
julianhermle <60342199+julianhermle@users.noreply.github.com>
2020-10-10 11:01:34 +0200

    query added

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ const connection = mysql.createConnection({
     console.log(error)
     }else{
       console.log("MySQL connected...")
+      connection.query("CREATE TABLE `benutzer` (`id` int(3) NOT NULL,`benutzername` varchar(30) NOT NULL,`passwort` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
+      connection.query("INSERT INTO `benutzer`(`id`, `benutzername`, `passwort`) VALUES (1,'test','123');");
     }
   });
 
@@ -32,3 +34,4 @@ app.listen(2222, () => {
 app.get("/", (req, res)=>{
    //res.send("Hello world");
 });
+
