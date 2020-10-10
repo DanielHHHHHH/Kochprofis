dae5ff504c9b4196437df6a984b273c35fa30620
julianhermle <60342199+julianhermle@users.noreply.github.com>
2020-10-10 11:42:33 +0200

    stuff for testing added

--- public/startScript.js
+++ public/startScript.js
@@ -37,3 +37,4 @@ button.addEventListener("click", (evt) => {
     evt.preventDefault();
 });
 */
+
--- server.js
+++ server.js
@@ -17,8 +17,10 @@ const connection = mysql.createConnection({
     console.log(error)
     }else{
       console.log("MySQL connected...")
+      /*
       connection.query("CREATE TABLE `benutzer` (`id` int(3) NOT NULL,`benutzername` varchar(30) NOT NULL,`passwort` varchar(30) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
       connection.query("INSERT INTO `benutzer`(`id`, `benutzername`, `passwort`) VALUES (1,'test','123');");
+      */
     }
   });
 
@@ -33,5 +35,20 @@ app.listen(2222, () => {
 
 app.get("/", (req, res)=>{
    //res.send("Hello world");
+});
+
+app.post("/benutzer", async (req, res) => {
+  const [
+    rows,
+  ] = await connection.execute(
+    "INSERT INTO benutzer (benutzername, passwort) VALUES (?, ?)",
+    [req.body.benutzername, req.body.kennwort]
+  );
+
+  res.json({
+    id: rows.insertId,
+    benutzername: req.body.benutzername,
+    kennwort: req.body.kennwort,
+  });
 });
 
