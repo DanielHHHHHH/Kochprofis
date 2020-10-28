9ed8e72cdf57a08a5d29d0d16af98b6d1243a1dd
julianhermle <60342199+julianhermle@users.noreply.github.com>
2020-10-28 10:55:10 +0100

    Testrezept eingefügt

--- database.sql
+++ database.sql
@@ -18,3 +18,11 @@ CREATE TABLE benutzer (
     passwort varchar(255) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO rezepte (name, rezepttext, autor)
+VALUES ("Kürbiskuchen","4 Eier, 400g Zucker, 200ml Öl verrühren. Nach und nach das mit dem Backpulver (1 Pack) vermischte und gesiebte Mehl (400g) sowie die gem. Mandeln (150g) einrühren. Das geraspelte Kürbisfleisch (400g) vorsichtig unterheben!
+In einer großen Kastenform bei 160 Grad ca. 75 Minuten backen.
+In der Form etwas abkühlen lassen und gaaaanz vorsichtig auf ein Kuchengitter stürzen.
+Dann nach Belieben mit Schokoguss überziehen und evtl. mit Mandelstückchen bestreuen.
+Mandeln und Kürbis können auch durch Nüsse und Zucchini ersetzt werden.
+Der Kuchen ist sehr lecker und saftig.", "Tom Kürbis");
